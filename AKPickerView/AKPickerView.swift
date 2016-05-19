d10ac0569cd791988a783d7208bf98807da1789c
Linda Morton <linda@hyperhyper.com>
2016-05-19 14:34:47 -0700

    Allow vertical scroll indicator to be turned off

--- AKPickerView/AKPickerView.swift
+++ AKPickerView/AKPickerView.swift
@@ -303,6 +303,12 @@ public class AKPickerView: UIView, UICollectionViewDataSource, UICollectionViewD
 				}()
 		}
 	}
+    
+    public var showsVerticalScrollIndicator: Bool = true {
+        didSet {
+            self.collectionView.showsVerticalScrollIndicator = showsVerticalScrollIndicator
+        }
+    }
 
 	// MARK: Readonly Properties
 	/// Readonly. Index of currently selected item.
